e2226e588dcecaf13587b635a3afe0b5cbb6da4d
utsavkuchhal <utsavkuchhal2420@gmail.com>
2020-08-15 01:05:01 +0530

    make it better

---
+++ client/build/precache-manifest.17e8da95cc44c8fd3449cb5a589d5dec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e913acbf378c5dc42ac50b5632c3ef9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "986992fee7fec83a395c",
+    "url": "/static/css/main.3384fc92.chunk.css"
+  },
+  {
+    "revision": "3a901dcbec1db8fd7ad9",
+    "url": "/static/js/2.862dae53.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.862dae53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "986992fee7fec83a395c",
+    "url": "/static/js/main.ecd4cbf8.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
