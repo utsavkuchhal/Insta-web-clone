23a140771990d1e048b1a8a7566bd5fa38c44d80
utsavkuchhal <utsavkuchhal2420@gmail.com>
2020-08-15 00:29:12 +0530

    make it better

--- app.js
+++ app.js
@@ -19,10 +19,10 @@ app.use(require("./routes/post"));
 app.use(require("./routes/user"));
 
 if(process.env.NODE_ENV=="production"){
-    app.use(express.static('my-app/build'))
+    app.use(express.static('client/build'))
     const path = require('path')
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'my-app','build','index.html'))
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
 }
 
